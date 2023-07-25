67682170a334cc58812ecf9729ac6b55f4476d67
kash1064 <kashiwabayuki@gmail.com>
2023-07-25 12:56:43 +0000

    Update from https://github.com/kash1064/Kaeru-no-Himitsukichi/commit/e67c2f479b00495e74abf7382d531615f60b998c

---
+++ file/EnableFulldump.ps1
@@ -0,0 +1,55 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+# Settings of Full memory dump
+$crashControlRegPath = "HKLM:System\CurrentControlSet\Control\CrashControl"
+$isExistKey = Test-Path -LiteralPath $crashControlRegPath
+if ($isExistKey -eq $False) {
+	New-Item -Path $parameterRegPath
+}
+New-ItemProperty -LiteralPath $CrashControlRegPath -Name "CrashDumpEnabled" -PropertyType "DWord" -Value "1" -Force
+New-ItemProperty -LiteralPath $CrashControlRegPath -Name "AutoReboot" -PropertyType "DWord" -Value "1" -Force
+New-ItemProperty -LiteralPath $CrashControlRegPath -Name "DumpFile" -PropertyType "ExpandString" -Value "%SystemRoot%\FULL_MEMORY.DMP" -Force
+New-ItemProperty -LiteralPath $CrashControlRegPath -Name "LogEvent" -PropertyType "DWord" -Value "1" -Force
+
+# Disable CrashOnCtrlScroll
+$parameterRegPaths = @("HKLM:System\CurrentControlSet\Services\i8042prt\Parameters",
+					  "HKLM:System\CurrentControlSet\Services\kbdhid\Parameters",
+					  "HKLM:System\CurrentControlSet\Services\hyperkbd\Parameters"
+					)
+foreach ($parameterRegPath in $parameterRegPaths) {
+	$isExistKey = Test-Path -LiteralPath $parameterRegPath
+	if ($isExistKey -eq $False) {
+		New-Item -Path $parameterRegPath
+	}
+	New-ItemProperty -LiteralPath $parameterRegPath -Name "CrashOnCtrlScroll" -PropertyType "DWord" -Value "0" -Force
+}
+
+# Setting alt dump key
+$parameterRegPaths = @("HKLM:System\CurrentControlSet\Services\i8042prt\crashdump",
+					  "HKLM:System\CurrentControlSet\Services\kbdhid\crashdump",
+					  "HKLM:System\CurrentControlSet\Services\hyperkbd\crashdump"
+					)
+foreach ($parameterRegPath in $parameterRegPaths) {
+	$isExistKey = Test-Path -LiteralPath $parameterRegPath
+	if ($isExistKey -eq $False) {
+		New-Item -Path $parameterRegPath
+	}
+	New-ItemProperty -LiteralPath $parameterRegPath -Name "Dump1Keys" -PropertyType "DWord" -Value "0x2" -Force
+	New-ItemProperty -LiteralPath $parameterRegPath -Name "Dump2Key" -PropertyType "DWord" -Value "0x3d" -Force
+}
+
+# Change PageFileSize
+$totalPhysicalMemSize = $([Math]::Round((Get-WmiObject Win32_OperatingSystem).TotalVisibleMemorySize / 1024))
+$freeStorageSizeofC = $([Math]::Round(((Get-PSDrive C).Free / 1024 / 1024)))
+$pageFileSize = $totalPhysicalMemSize + 400
+$pageFileSetting = "c:\pagefile.sys $pageFileSize $pageFileSize"
+if (($freeStorageSizeofC -gt $pageFileSize) -eq $True) {
+	New-ItemProperty -LiteralPath "HKLM:System\CurrentControlSet\Control\Session Manager\Memory Management" -Name "PagingFiles" -PropertyType "MultiString" -Value $pageFileSetting -Force
+} else {
+	Write-Warning "C drive space is too small."
+}

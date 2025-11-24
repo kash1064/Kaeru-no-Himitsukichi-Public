32c3556c62da87c4858051516097e73add7df3f9
kash1064 <kashiwabayuki@gmail.com>
2025-11-24 12:41:44 +0000

    Update from https://github.com/kash1064/Kaeru-no-Himitsukichi/commit/dbdb358f0bb8fff7557522c4e29293265aa9424d

--- sw.js
+++ sw.js
@@ -42,14 +42,14 @@ self.__precacheManifest = [
     "url": "532a2f07-92db97c0addf07d5cb73.js"
   },
   {
-    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-165f5b462b8db560be5f.js"
+    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-9402788243542893fe79.js"
   },
   {
-    "url": "app-e62788978703059289bd.js"
+    "url": "app-8d44fd3e117392cacf03.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d06e6ebc8302ad0ceaf775a6e13f87d8"
+    "revision": "8e834fed7cdaaa918a3f2584e602f0e3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fd4fb51a6fac1c18bdde.js"
@@ -155,7 +155,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-e62788978703059289bd.js`))) {
+  if (!resources || !(await caches.match(`/app-8d44fd3e117392cacf03.js`))) {
     return await fetch(event.request)
   }
 
